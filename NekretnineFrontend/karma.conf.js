b2f0bc5c6fa750edd5264ad92cb591664086ac1a
Mehmed <mehmed_new@hotmail.com>
2022-02-18 20:05:21 +0100

    Dodavanje angular projekta

--- NekretnineFrontend/karma.conf.js
+++ NekretnineFrontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rs1-seminarski'),
+      dir: require('path').join(__dirname, './coverage/nekretnine-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
